e7a3629a1e34bded30e9acc41f8317cb80cb5e31
affect <affect@09fe5ad0-ae32-4082-b760-a95d3f25b491>
2012-03-03 14:34:06 +0000

    7. Added product, seller, rating and transaction tables

--- upload/admin/model/module/multiseller.php
+++ upload/admin/model/module/multiseller.php
@@ -1,23 +1,67 @@
 <?php
 class ModelModuleMultiseller extends Model {
 	public function createTable() {
-		$createTable = "
-			CREATE TABLE " . DB_PREFIX . "multiseller (
-             `id` int(11) NOT NULL AUTO_INCREMENT,
-             `id_product` int(11) NOT NULL,
-             `id_customer` int(10) UNSIGNED DEFAULT NULL,
-             `name` varchar(32) NOT NULL DEFAULT '',
-             `email` varchar(128) NOT NULL DEFAULT '',
-             `comment` text NOT NULL,
-             `create_time` int(11) NOT NULL DEFAULT '0',
-             `display` tinyint(1) NOT NULL DEFAULT '0',
-        	PRIMARY KEY (`id`)) default CHARSET=utf8";
+		$sql = "
+			CREATE TABLE `" . DB_PREFIX . "ms_product` (
+             `product_id` int(11) NOT NULL,
+             `seller_id` int(11) DEFAULT NULL,
+             `number_sold` int(11) UNSIGNED DEFAULT '0',
+             `review_status` tinyint UNSIGNED DEFAULT '0',
+        	PRIMARY KEY (`product_id`,`seller_id`)) default CHARSET=utf8";
         
-        //$this->db->query($createTable);
+        $this->db->query($sql);
+        
+		$sql = "
+			CREATE TABLE `" . DB_PREFIX . "ms_seller` (
+             `seller_id` int(11) NOT NULL AUTO_INCREMENT,
+             `company` VARCHAR(32) NOT NULL DEFAULT '',
+             `website` VARCHAR(2083) NOT NULL DEFAULT '',
+             `description` TEXT NOT NULL DEFAULT '',
+			 `country_id` INT(11) NOT NULL DEFAULT '0',
+			 `avatar` VARCHAR(255) DEFAULT NULL,
+			 `date_created` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+			 `status` TINYINT UNSIGNED NOT NULL DEFAULT '0',
+        	PRIMARY KEY (`seller_id`)) default CHARSET=utf8";
+        
+        $this->db->query($sql);
+        
+		$sql = "
+			CREATE TABLE `" . DB_PREFIX . "ms_product_rating` (
+             `product_id` int(11) NOT NULL,
+             `seller_id` int(11) DEFAULT NULL,
+             `customer_id` int(11) NOT NULL,
+             `rating_value` TINYINT NOT NULL,
+			 `date_created` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+        	PRIMARY KEY (`product_id`,`seller_id`,`customer_id`)) default CHARSET=utf8";
+        
+        $this->db->query($sql);
+        
+		$sql = "
+			CREATE TABLE `" . DB_PREFIX . "ms_transaction` (
+             `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
+             `amount` DECIMAL(15,4) NOT NULL DEFAULT '0',
+             `seller_id` int(11) NOT NULL,
+             `order_id` int(11) DEFAULT NULL,
+             `status_id` int(11) NOT NULL DEFAULT '0',
+             `currency_id` int(11) NOT NULL,
+             `currency_code` VARCHAR(3) NOT NULL,
+             `currency_value` DECIMAL(15,8) NOT NULL,
+             `commission` DECIMAL(4,2) NOT NULL DEFAULT '0',				
+             `description` TEXT NOT NULL DEFAULT '',			              
+			 `date_created` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+			 `date_modified` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+        	PRIMARY KEY (`transaction_id`)) default CHARSET=utf8";
+        
+        $this->db->query($sql);        
 	}
 	
 	public function dropTable() {
-		$dropTable = "DROP TABLE IF EXISTS " . DB_PREFIX . "multiseller";
-		//$this->db->query($dropTable);
+		$sql = "DROP TABLE IF EXISTS
+				`" . DB_PREFIX . "ms_product`,
+				`" . DB_PREFIX . "ms_seller`,
+				`" . DB_PREFIX . "ms_product_rating`,
+				`" . DB_PREFIX . "ms_transaction`";
+								
+		$this->db->query($sql);
     }
 }
