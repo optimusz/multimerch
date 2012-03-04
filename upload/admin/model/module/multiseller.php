4bdc9c664c207a86a96f75d779e81d4f57c760e3
alboro <alboro@09fe5ad0-ae32-4082-b760-a95d3f25b491>
2012-03-04 23:34:01 +0000

    db planning

--- upload/admin/model/module/multiseller.php
+++ upload/admin/model/module/multiseller.php
@@ -5,9 +5,9 @@ class ModelModuleMultiseller extends Model {
 			CREATE TABLE `" . DB_PREFIX . "ms_product` (
              `product_id` int(11) NOT NULL,
              `seller_id` int(11) DEFAULT NULL,
-             `number_sold` int(11) UNSIGNED DEFAULT '0',
-             `review_status` tinyint UNSIGNED DEFAULT '0',
-        	PRIMARY KEY (`product_id`,`seller_id`)) default CHARSET=utf8";
+             `number_sold` int(11) UNSIGNED NOT NULL DEFAULT '0',
+             `review_status_id` tinyint UNSIGNED NOT NULL DEFAULT '1',
+        	PRIMARY KEY (`product_id`)) default CHARSET=utf8";
         
         $this->db->query($sql);
         
@@ -18,9 +18,9 @@ class ModelModuleMultiseller extends Model {
              `website` VARCHAR(2083) NOT NULL DEFAULT '',
              `description` TEXT NOT NULL DEFAULT '',
 			 `country_id` INT(11) NOT NULL DEFAULT '0',
-			 `avatar` VARCHAR(255) DEFAULT NULL,
+			 `avatar_path` VARCHAR(255) DEFAULT NULL,
 			 `date_created` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-			 `status` TINYINT UNSIGNED NOT NULL DEFAULT '0',
+			 `seller_status_id` TINYINT UNSIGNED NOT NULL DEFAULT '1',
         	PRIMARY KEY (`seller_id`)) default CHARSET=utf8";
         
         $this->db->query($sql);
@@ -42,7 +42,7 @@ class ModelModuleMultiseller extends Model {
              `amount` DECIMAL(15,4) NOT NULL DEFAULT '0',
              `seller_id` int(11) NOT NULL,
              `order_id` int(11) DEFAULT NULL,
-             `status_id` int(11) NOT NULL DEFAULT '0',
+             `transaction_status_id` int(11) NOT NULL,
              `currency_id` int(11) NOT NULL,
              `currency_code` VARCHAR(3) NOT NULL,
              `currency_value` DECIMAL(15,8) NOT NULL,
